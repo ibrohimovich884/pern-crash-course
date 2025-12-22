ce935702a245f9ba6177942104b2bac478e209d2
ibrohimovich884 <bkbekmirzayev@gmail.com>
2025-12-22 12:39:02 +0500

    backend path update

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const AUTH_KEY = "posgrestore-auth-token";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_URL;
 export const useAuthStore = create((set, get) => {
   // Check if token exists and is valid
 
--- frontend/src/store/useProductStore.js
+++ frontend/src/store/useProductStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const useProductStore = create((set, get) => ({
   products: [],
